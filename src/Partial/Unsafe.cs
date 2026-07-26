091fc9e205219285fc806f76aa2725f467a2c381
0x1 <kevin.francart3@gmail.com>
2026-07-26 14:40:19 +0200

    feat: add C# FFI implementations

---
+++ src/Partial/Unsafe.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace Partial.Unsafe;
+
+public static class FFI {
+    public static object _UnsafePartial(object arg1) => throw new NotImplementedException("Not implemented: _UnsafePartial");
+}
